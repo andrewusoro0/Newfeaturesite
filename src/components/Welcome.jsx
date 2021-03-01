1cd2917b116f4142769ae76ad20540e293c33f68
KingsleyUsoroeno <kingsleyenou@gmail.com>
2021-03-01 15:05:38 +0100

    ft-personal-site

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+
+
 
 
 
@@ -8,8 +10,8 @@ function Welcome(){
     return(
         <div>
         <ul className="download">
-        <li className="resume">
-        <a className="text-pdf" href={'https://docs.google.com/document/d/1aycc1DTOdWASK6cjAF6ZLyhIaXIfQ9jjJ-IfTjRJD0k/edit?usp=sharing'}>Resume.pdf</a>
+        <li className="resume" id="button">
+        <a className="text-pdf" id="resume" href={"https://drive.google.com/file/d/1kvP6AwRMizQXo6o5cKOhmwNp2QfDVGdP/view?usp=sharing"} download="AndyTechResume">Resume.pdf</a>
         </li>
         </ul>
         <h2 className="intro-text">Hi, my name is Andrew Usoro and I am a software developer from Nigeria.</h2>
